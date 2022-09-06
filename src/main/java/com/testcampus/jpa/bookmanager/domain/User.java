a5a93b71143e71401ca7556fac3236110ab842bc
kyungminJ <jkm210@mkinternet.com>
2022-09-06 17:58:17 +0900

    Lombok example

---
+++ src/main/java/com/testcampus/jpa/bookmanager/domain/User.java
@@ -0,0 +1,19 @@
+package com.testcampus.jpa.bookmanager.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Builder
+public class User {
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
