c9b7d07d8a62e6567003c9c2f8ac78aa076db675
kyungminJ <jkm210@mkinternet.com>
2022-09-06 16:45:24 +0900

    initial

---
+++ src/test/java/com/testcampus/jpa/bookmanager/controller/HelloWorldControllerTest.java
@@ -0,0 +1,25 @@
+package com.testcampus.jpa.bookmanager.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest
+class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloWorld() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello-world"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello world"));
+    }
+}
