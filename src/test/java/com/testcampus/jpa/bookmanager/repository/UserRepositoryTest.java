4313ce93cd48e43f2083d8495c00578b45a31d5e
kyungminJ <jkm210@mkinternet.com>
2022-09-07 16:21:13 +0900

    JPA Study1

--- src/test/java/com/testcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/testcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.testcampus.jpa.bookmanager.repository;
 
 import com.testcampus.jpa.bookmanager.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +18,20 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        userRepository.save(new User());
+//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+//        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 3L, 5L));
+//        users.forEach(System.out::println);
 
-        System.out.println(userRepository.findAll());
+//        User user1 = new User("jack", "jack@naver.com");
+//        User user2 = new User("david", "david@naver.com");
+//        userRepository.saveAll(Lists.newArrayList(user1, user2));
+//        userRepository.save(user1);
+//        List<User> users = userRepository.findAll();
+//        users.forEach(System.out::println);
+
+//        User user = userRepository.getOne(1L);
+        User user = userRepository.findById(1L).orElse(null);
+        System.out.println(user);
     }
 }
